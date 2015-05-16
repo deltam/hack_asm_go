7497214c02314ea042c990a760aad6f1b05745f1
MISUMI Masaru(deltam) <deltam@gmail.com>
2015-05-16 14:40:16 +0900

    made no symbol Assembler

--- cmd/Assembler/Assembler.go
+++ cmd/Assembler/Assembler.go
@@ -1,44 +1,41 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    asm "github.com/deltam/hack_asm_go"
-    "os"
+	"bufio"
+	"fmt"
+	asm "github.com/deltam/hack_asm_go"
+	"os"
 )
 
 func main() {
-    var fp *os.File
-    var err error
+	var fp *os.File
+	var err error
 
-    if len(os.Args) < 2 {
-        panic("Usage: Assembler Prog.asm")
-    } else {
-        fp, err = os.Open(os.Args[1])
-        if err != nil {
-            panic(err)
-        }
-        defer fp.Close()
-    }
+	if len(os.Args) < 2 {
+		panic("Usage: Assembler Prog.asm")
+	} else {
+		fp, err = os.Open(os.Args[1])
+		if err != nil {
+			panic(err)
+		}
+		defer fp.Close()
+	}
 
-    scanner := bufio.NewScanner(fp)
-    parser := asm.NewParser(scanner)
+	scanner := bufio.NewScanner(fp)
+	parser := asm.NewParser(scanner)
 
-    fmt.Println(parser)
-
-    // コマンド処理
-    for ; parser.HasMoreCommands(); parser.Advance() {
-        fmt.Println(parser.CurrentCommand())
-        fmt.Println("type: " + parser.CommandType())
-        if parser.CommandType() == asm.A_COMMAND || parser.CommandType() == asm.L_COMMAND {
-            fmt.Println("address: " + parser.Symbol())
-        }
-        if parser.CommandType() == asm.C_COMMAND {
-            fmt.Println("cmp: " + parser.Comp())
-            fmt.Println("dst: " + parser.Dest())
-            fmt.Println("jmp: " + parser.Jump())
-            cmd := asm.NewCode(*parser)
-            fmt.Println("BINARY:  " + cmd.BinCode())
-        }
-    }
+	// コマンド処理
+	for ; parser.HasMoreCommands(); parser.Advance() {
+		if parser.CommandType() == asm.A_COMMAND || parser.CommandType() == asm.L_COMMAND {
+			adrs, err := parser.Address()
+			if err == nil {
+				fmt.Println(adrs)
+			} else {
+				panic("error address")
+			}
+		} else if parser.CommandType() == asm.C_COMMAND {
+			cmd := asm.NewCode(*parser)
+			fmt.Println(cmd.BinCode())
+		}
+	}
 }
--- parse.go
+++ parse.go
@@ -1,97 +1,117 @@
 package hack_asm_go
 
 import (
-    "bufio"
-    "strings"
+	"bufio"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 type Parser struct {
-    Lines   int
-    Src     []string
-    current int
+	Lines   int
+	Src     []string
+	current int
 }
 
 const (
-    A_COMMAND = "A"
-    C_COMMAND = "C"
-    L_COMMAND = "L"
+	A_COMMAND = "A"
+	C_COMMAND = "C"
+	L_COMMAND = "L"
 )
 
 func NewParser(scanner *bufio.Scanner) *Parser {
-    var src []string
-    var lines int
-    var text string
-    for scanner.Scan() {
-        text = scanner.Text()
-        text = strings.Replace(text, " ", "", -1) // remove space
-        text = strings.Split(text, "//")[0] // remove comment
-        if text != "" {
-            src = append(src, text)
-            lines++
-        }
-    }
-    return &Parser{
-        Lines:   lines,
-        Src:     src,
-        current: 0,
-    }
+	var src []string
+	var lines int
+	var text string
+	for scanner.Scan() {
+		text = scanner.Text()
+		text = strings.Replace(text, " ", "", -1) // remove space
+		text = strings.Split(text, "//")[0]       // remove comment
+		if text != "" {
+			src = append(src, text)
+			lines++
+		}
+	}
+	return &Parser{
+		Lines:   lines,
+		Src:     src,
+		current: 0,
+	}
 }
 
 // まだコマンドが残っているか
 func (parser Parser) HasMoreCommands() bool {
-    return parser.Lines > parser.current
+	return parser.Lines > parser.current
 }
 
 // 現在のコマンドをひとつ進める
 func (parser *Parser) Advance() {
-    parser.current++
+	parser.current++
 }
 
 // 現在のコマンド文字列を返す
 func (parser Parser) CurrentCommand() string {
-    return parser.Src[parser.current]
+	return parser.Src[parser.current]
 }
 
 // コマンドの種類を返す
 func (parser Parser) CommandType() string {
-    var cmd = parser.CurrentCommand()
-    switch {
-    case cmd[0] == '@':
-        return A_COMMAND
-    case cmd[0] == '(':
-        return L_COMMAND
-    default:
-        return C_COMMAND
-    }
+	var cmd = parser.CurrentCommand()
+	switch {
+	case cmd[0] == '@':
+		return A_COMMAND
+	case cmd[0] == '(':
+		return L_COMMAND
+	default:
+		return C_COMMAND
+	}
 }
 
 func (parser Parser) Symbol() string {
-    if parser.CommandType() == A_COMMAND {
-        return parser.CurrentCommand()[1:]
-    } else if parser.CommandType() == L_COMMAND {
-        return strings.Trim(parser.CurrentCommand(), "()")
-    }
-    return "NULL"
+	if parser.CommandType() == A_COMMAND {
+		return parser.CurrentCommand()[1:]
+	} else if parser.CommandType() == L_COMMAND {
+		return strings.Trim(parser.CurrentCommand(), "()")
+	}
+	return "NULL"
+}
+
+// 命令を分割する
+// tokens[0] = cmp
+// tokens[1] = dest
+// tokens[2] = jmp
+func (parser Parser) split(command string) [3]string {
+	var tokens [3]string
+	if strings.Index(command, "=") != -1 {
+		tokens[1] = strings.Split(command, "=")[0]
+		command = strings.Split(command, "=")[1]
+	}
+	if strings.Index(command, ";") != -1 {
+		tokens[0] = strings.Split(command, ";")[0]
+		tokens[2] = strings.Split(command, ";")[1]
+	} else {
+		tokens[0] = command
+	}
+	return tokens
 }
 
 func (parser Parser) Dest() string {
-    if strings.Index(parser.CurrentCommand(), "=") != -1 {
-        return strings.Split(parser.CurrentCommand(), "=")[0]
-    }
-    return ""
+	tokens := parser.split(parser.CurrentCommand())
+	return tokens[1]
 }
 
 func (parser Parser) Comp() string {
-    if strings.Index(parser.CurrentCommand(), "=") != -1 {
-        cmpJmp := strings.Split(parser.CurrentCommand(), "=")[1]
-        return strings.Split(cmpJmp, ";")[0]
-    }
-    return ""
+	tokens := parser.split(parser.CurrentCommand())
+	return tokens[0]
 }
 
 func (parser Parser) Jump() string {
-    if strings.Index(parser.CurrentCommand(), ";") != -1 {
-        return strings.Split(parser.CurrentCommand(), ";")[1]
-    }
-    return ""
+	tokens := parser.split(parser.CurrentCommand())
+	return tokens[2]
+}
+
+// A命令のバイナリ表現を返す
+func (parser Parser) Address() (string, error) {
+	adrs, err := strconv.ParseInt(parser.Symbol(), 10, 16)
+	return fmt.Sprintf("0%015b", adrs), err
 }
